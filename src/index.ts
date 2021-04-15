f03990bae3d614ac246689db6d8f4f7ff171ea8e
narutopig <redgerxu@gmail.com>
2021-04-14 21:42:11 -0700

    CLI Changes
    Added yargs for command line arguments.
    Added some extra things for the parser.
    Added support for reading .bruh files.
    package and tsconfig.json changes

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
-import * as log from "./log";
+#!/usr/bin/env node
+
+import yargs from "yargs";
+import path from "path";
 import parse from "./parse";
+import fs from "fs";
 
-let filename: string | null = process.argv[2];
-let mode: string = "file";
-let code: string = "";
+const args = yargs
+    .options({
+        mode: { type: "string", demandOption: true, alias: "m" },
+        file: { type: "string", demandOption: false, alias: "f" },
+        code: { type: "string", demandOption: false, alias: "c" },
+    })
+    .check((argv) => {
+        // currently broken (runs stuff but doesnt have message if incorrect args)
+        if (argv.file && !argv.code) return true;
+        if (!argv.file && argv.code) return true;
+        if (argv.file && argv.file.endsWith(".bruh")) return true;
+        if (argv.mode === "file" || argv.mode === "code") return true;
+        if (argv.mode === "code" || argv.code?.trim().length !== 0) return true;
+        if (argv.file!.length > 0 || argv.code!.length > 0) return true;
+        throw new Error(
+            "You must provide only either BruhLang code or a BruhLang file (.bruh)."
+        );
+    }).argv;
 
-if (!filename) {
-    log.error("No input file.")
-    process.exit(1);
-}
+const { mode, file } = args;
+let { code } = args;
 
-if (filename === "-s") {
-    mode = "string";
-    filename = null;
-    code = process.argv[3] || "";
-}
-
-if (mode === "file") {
-    // unimplemented
-} else if (mode === "string") {
+if (mode === "file" && file) {
+    const sourcePath = path.resolve(process.cwd(), file);
+    const source = fs.readFileSync(sourcePath, { encoding: "utf-8" });
+    code = source.trim().split("\n").join("");
+    parse(code);
+} else if (mode === "code" && code) {
     parse(code);
 }
